499447da91225c3f62f5601e6d3eb470da1f7006
zyhwjl2022 <117875453+zyhwjl2022@users.noreply.github.com>
2024-05-11 15:25:06 +0800

    Create dftherh5.js

---
+++ zyhwjl/dftherh5.js
@@ -0,0 +1,20 @@
+/* 
+脚本功能: dftherh5解密
+
+[rewrite local]
+^https:\/\/dftherh5.hcjvdum.com\/api\/app\/ping\/config url script-request-header https://raw.githubusercontent.com/zyhwjl2022/QuantumultX/master/zyhwjl/dftherh5.js
+
+[MITM]
+hostname = dftherh5.hcjvdum.com
+
+*/
+const $ = new Env("dftherh5解密");
+
+!(async () => {
+  const session = {};
+  session.url = $request.url;
+  session.body = $request.body;
+  session.headers = $request.headers;
+  $.log(JSON.stringify(session));
+
+}
