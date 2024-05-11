c2d709ab46affb7e18d21f27d5fbbb0f3ed55e31
zyhwjl2022 <117875453+zyhwjl2022@users.noreply.github.com>
2024-05-11 23:02:03 +0800

    Update dftherh5.js

--- zyhwjl/dftherh5.js
+++ zyhwjl/dftherh5.js
@@ -15,112 +15,109 @@ let body = $response.body
 var jsonData = JSON.parse(body);
 var data = jsonData.data
 
-//const CryptoJS = require('crypto-js')
-//const { toByteArray, fromByteArray } = require('base64-js')
-const url = 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js';
+/**
+ * @supported A2BCC90F0711
+ */
 
-const CryptoJS = fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.text();
-  })
-  .then(script => {
-    return script
-  })
-  .catch(error => console.error('There was a problem with the fetch operation:', error));
-const burl = 'https://unpkg.com/base64-js@1.5.1/index.js';
+// Function to load external JavaScript files
+function loadScript(url) {
+  const script = `
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '${url}', false);
+    xhr.send(null);
+    eval(xhr.responseText);
+  `;
+  eval(script);
+}
 
-const { toByteArray, fromByteArray } = fetch(burl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.text();
-  })
-  .then(script => {
-    return script
-  })
-  .catch(error => console.error('There was a problem with the fetch operation:', error));
+// Load CryptoJS library
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.2.0/crypto-js.min.js');
+// Load base64-js library
+loadScript('https://cdn.jsdelivr.net/npm/base64-js@1.5.1/base64js.min.js');
 
+// Your decoding function
 function decodeHttpResponseData(cipher) {
-  const key = 'vEukA&w15z4VAD3kAY#fkL#rBnU!WDhN' //  rTukA&w1578VAD3#AY3fkL#rBnU^DDuO 之前的
-  const nonceLength = 12
+  const key = 'vEukA&w15z4VAD3kAY#fkL#rBnU!WDhN'; //  rTukA&w1578VAD3#AY3fkL#rBnU^DDuO 之前的
+  const nonceLength = 12;
   // cipher Base64解码
-  const cipherBytes = Array.from(toByteArray(cipher))
-  const nonce = cipherBytes.splice(0, nonceLength)
-  const enCodeKey = encodeUtf8(key)
+  const cipherBytes = Array.from(base64js.toByteArray(cipher));
+  const nonce = cipherBytes.splice(0, nonceLength);
+  const enCodeKey = encodeUtf8(key);
   // 组装largeShaRaw数组计算KEY
-  const largeShaRaw = [...enCodeKey, ...nonce]
-  const largeShaRawMid = parseInt(largeShaRaw.length / 2)
-  const largeShaRawByteArray = fromByteArray(largeShaRaw)
-  const base64ShaRaw = CryptoJS.enc.Base64.parse(largeShaRawByteArray)
-  const msgKeyLarge = Str16ToBytes(CryptoJS.SHA256(base64ShaRaw).toString())
-  const msgKey = msgKeyLarge.splice(8, 16)
+  const largeShaRaw = [...enCodeKey, ...nonce];
+  const largeShaRawMid = parseInt(largeShaRaw.length / 2);
+  const largeShaRawByteArray = base64js.fromByteArray(largeShaRaw);
+  const base64ShaRaw = CryptoJS.enc.Base64.parse(largeShaRawByteArray);
+  const msgKeyLarge = Str16ToBytes(CryptoJS.SHA256(base64ShaRaw).toString());
+  const msgKey = msgKeyLarge.splice(8, 16);
   // 计算sha256a
-  const shaRawA = [...msgKey, ...largeShaRaw.splice(0, largeShaRawMid)]
-  const sha256aByteArray = fromByteArray(shaRawA)
-  const base64Sha256a = CryptoJS.enc.Base64.parse(sha256aByteArray)
-  const sha256a = Str16ToBytes(CryptoJS.SHA256(base64Sha256a).toString())
+  const shaRawA = [...msgKey, ...largeShaRaw.splice(0, largeShaRawMid)];
+  const sha256aByteArray = base64js.fromByteArray(shaRawA);
+  const base64Sha256a = CryptoJS.enc.Base64.parse(sha256aByteArray);
+  const sha256a = Str16ToBytes(CryptoJS.SHA256(base64Sha256a).toString());
   // 计算sha256b
-  const shaRawB = [...largeShaRaw, ...msgKey]
-  const sha256bByteArray = fromByteArray(shaRawB)
-  const base64Sha256b = CryptoJS.enc.Base64.parse(sha256bByteArray)
-  const sha256b = Str16ToBytes(CryptoJS.SHA256(base64Sha256b).toString())
+  const shaRawB = [...largeShaRaw, ...msgKey];
+  const sha256bByteArray = base64js.fromByteArray(shaRawB);
+  const base64Sha256b = CryptoJS.enc.Base64.parse(sha256bByteArray);
+  const sha256b = Str16ToBytes(CryptoJS.SHA256(base64Sha256b).toString());
   // 计算aesKey
   const aesKey = [
     ...sha256a.splice(0, 8),
     ...sha256b.splice(8, 16),
-    ...sha256a.splice(16, 24)]
+    ...sha256a.splice(16, 24)
+  ];
   // 计算aesIV
   const aesIV = [
     ...sha256b.splice(0, 4),
     ...sha256a.splice(4, 8),
-    ...sha256b.splice(8, 12)]
+    ...sha256b.splice(8, 12)
+  ];
   // cipher Data 转字节
-  const data = fromByteArray(cipherBytes)
+  const data = base64js.fromByteArray(cipherBytes);
   // aesKey 转字节 后Base64解码
-  const akey = CryptoJS.enc.Base64.parse(fromByteArray(aesKey))
+  const akey = CryptoJS.enc.Base64.parse(base64js.fromByteArray(aesKey));
   // aesIV 转字节 后Base64解码
-  const iv = CryptoJS.enc.Base64.parse(fromByteArray(aesIV))
+  const iv = CryptoJS.enc.Base64.parse(base64js.fromByteArray(aesIV));
   // AES解码后转utf8字符串
-  const wordArrayData = CryptoJS.AES.decrypt(data, akey,
-    { iv, mode: CryptoJS.mode.CBC })
-  return wordArrayData.toString(CryptoJS.enc.Utf8)
+  const wordArrayData = CryptoJS.AES.decrypt(data, akey, { iv, mode: CryptoJS.mode.CBC });
+  return wordArrayData.toString(CryptoJS.enc.Utf8);
 
   function Str16ToBytes(str) {
-    let pos = 0
-    let len = str.length
+    let pos = 0;
+    let len = str.length;
     if (len % 2 !== 0) {
-      return null
+      return null;
     }
-    len /= 2
-    const hexA = []
+    len /= 2;
+    const hexA = [];
     for (let i = 0; i < len; i++) {
-      const s = str.substr(pos, 2)
-      const v = parseInt(s, 16)
-      hexA.push(v)
-      pos += 2
+      const s = str.substr(pos, 2);
+      const v = parseInt(s, 16);
+      hexA.push(v);
+      pos += 2;
     }
-    return hexA
+    return hexA;
   }
 
   function encodeUtf8(text) {
-    const code = encodeURIComponent(text)
-    const bytes = []
+    const code = encodeURIComponent(text);
+    const bytes = [];
     for (let i = 0; i < code.length; i++) {
-      const c = code.charAt(i)
+      const c = code.charAt(i);
       if (c === '%') {
-        const hex = code.charAt(i + 1) + code.charAt(i + 2)
-        const hexVal = parseInt(hex, 16)
-        bytes.push(hexVal)
-        i += 2
-      } else { bytes.push(c.charCodeAt(0)) }
+        const hex = code.charAt(i + 1) + code.charAt(i + 2);
+        const hexVal = parseInt(hex, 16);
+        bytes.push(hexVal);
+        i += 2;
+      } else {
+        bytes.push(c.charCodeAt(0));
+      }
     }
-    return bytes
+    return bytes;
   }
 }
+
+
 function parseM3u8(data) {
   // 初始化enkey 值 重写m3u8 enkey 链接不然不能访问
   const enkeyUri = 'http://localhost:3881/api/h5app/media/enkey'
@@ -149,5 +146,6 @@ function stringToUint8Array(str) {
 }
 data = decodeHttpResponseData(data)
 console.log(data)
+//jsonData.data = data
 
 $done(JSON.stringify(jsonData))
